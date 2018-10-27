baf29f409eb34825d4b991a063e6bdca1b53e69d
Ravi Soni <codespartan09@gmail.com>
2018-10-28 01:19:28 +0530

    fix(app.js): Fix db connection url
    Add support for env variables

--- src/app.js
+++ src/app.js
@@ -3,8 +3,15 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
+const dburi =
+  process.env.NODE_ENV === 'development'
+    ? 'mongodb://localhost:27017/uno_server_v1'
+    : `mongodb://${process.env.TEST_DB_USER}:${process.env.TEST_DB_PASSWORD}@${
+        process.env.TEST_DB_CONNECTION_ADDRESS
+      }`;
+
 mongoose.connect(
-  process.env.MONGODB_URI,
+  dburi,
   { useNewUrlParser: true }
 );
 mongoose.connection.on('error', err => {
